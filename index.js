93a4ed973f2212377249613cee3eb111d6cac52f
B-Rabbit7 <snouri3@my.bcit.ca>
2023-10-15 19:13:24 -0700

    meep

--- index.js
+++ index.js
@@ -1,22 +1,78 @@
 const responseDiv = document.getElementById('response');
-function myFunc(){
-    const names = ['Sara Brown', 'John Smith', 'Elon Musk'];
-    const dateOfBirths = ['1901-01-01', '1941-01-01', '1999-01-01'];
+const queryInput = document.getElementById('queryInput');
+const querySubmit = document.getElementById('query');
 
-    names.forEach((name, index) => {
-        console.log(name)
-        const data = JSON.stringify({ name, dateOfBirth: dateOfBirths[index] });
-        console.log(data);
-        const xhttp = new XMLHttpRequest();
-        xhttp.open('POST', 'http://localhost:3000/insert', true);
-        xhttp.setRequestHeader('Content-Type', 'application/json');
-        
-        xhttp.onreadystatechange = function () {
-            if (xhr.readyState === 4 && xhr.status === 200) {
-              const response = JSON.parse(xhr.responseText);
-              console.log(response);
-            }
-          };
-          xhttp.send(data);
-    });
-};
+const xhttp = new XMLHttpRequest();
+
+const endpoint = 'https://backend-eaf1.onrender.com/'
+const make_route = 'make'
+const add_route = 'add';
+const get_route = 'patients/'
+const params = '?request='
+
+
+function myFunc() {
+  console.log('entered myFunc');
+  xhttp.open('POST', endpoint + make_route, true);
+  xhttp.send();
+  xhttp.onreadystatechange = function () {
+    console.log('onreadystatechange:', xhttp.readyState, xhttp.status);
+    if (xhttp.readyState === 4) {
+      console.log('Response received');
+      if (xhttp.status === 200) {
+        console.log('Response status 200');
+        const response = JSON.parse(xhttp.responseText);
+        console.log(response);
+      }
+    }
+  };
+}
+
+function getQuery() {
+  console.log("Quering...");
+  const queryValue = queryInput.value;
+  const words = queryValue.split(" ");
+  if (words[0] === "SELECT") {
+    xhttp.open("GET", endpoint + get_route + params + queryValue, true);
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+      if (xhttp.readyState === 4) {
+        console.log('Response received');
+        if (xhttp.status === 200) {
+          console.log('Response status 200');
+          const response = JSON.parse(xhttp.responseText);
+          console.log(response);
+          responseDiv.innerText = JSON.stringify(response, null, 2);
+        } else {
+          responseDiv.innerText = 'Error: ' + xhttp.statusText;
+        }
+      }
+    };
+  } else if(words[0] === "INSERT") {
+    console.log("in here")
+    const data = {
+      request: queryValue
+  };
+    xhttp.open("POST", endpoint + add_route, true);
+    xhttp.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+    xhttp.send(JSON.stringify(data));
+    xhttp.onreadystatechange = function () {
+      if (xhttp.readyState === 4) {
+        console.log('Response received');
+        if (xhttp.status === 200) {
+          console.log('Response status 200');
+          const response = JSON.parse(xhttp.responseText);
+          console.log(response);
+          responseDiv.innerText = JSON.stringify(response, null, 2);
+        } else {
+          responseDiv.innerText = 'Error: ' + xhttp.statusText;
+        }
+      }
+    };
+  } else if(words.length > 0) {
+    console.log("Not Query word", words.length)
+  } else{
+    console.log("No words Entered")
+  }
+
+}
